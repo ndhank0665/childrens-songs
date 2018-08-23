3e7850bc0bbce4da6ea21a4da16689e495cd09fe
ndhank0665 <42582465+ndhank0665@users.noreply.github.com>
2018-08-23 12:50:49 -0400

    Add files via upload

---
+++ OldMacDonald2.java
@@ -0,0 +1,74 @@
+/**
+ * This program sings the song Old MacDonald Had a Farm.
+ *
+ * Version 2 - singIt() method eliminates duplicate code and make it easier to add more animals.
+ * 
+ * @author Jon Cooper
+ * @version August 18, 2016 (v0)
+ */
+
+
+public class OldMacDonald2 
+{
+    /**
+     * sings line 1
+     * 
+     * @param eieio Sings the first line
+     */
+    public static void eieio() 
+    { 
+        System.out.println("Old MacDonald had a farm, E I E I O.");
+    }
+
+    /** 
+     * sings line 2
+     * 
+     * @param hadA Sings the second line
+     */
+    public static void hadA(String animal) 
+    { 
+        System.out.println("And on that farm he had a " + animal + ", E I E I O.");
+    }
+
+    /**
+     * sings lines 3 and 4
+     * 
+     * @param withA Sings the third and the fourth line
+     */
+    public static void withA(String sound) 
+    { 
+        System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
+        System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
+    }
+
+    /**
+     * Describes the variables
+     * 
+     * @param singIt This describes all the variables in the program
+     */
+    public static void singIt(String animal, String sound) 
+    {
+        eieio();
+        hadA(animal);
+        withA(sound);
+        eieio();
+        System.out.println();
+    }
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
+
+        singIt("duck", "quack");
+        singIt("cow", "moo");
+        singIt("horse", "neigh");
+        singIt("platypus","gdggdggddgdg");
+        
+        // now see how easy it is to add some more animals  
+        
+        
+        // modify the methods above to get quotation marks around the animal sounds
+    }
+    
+}
